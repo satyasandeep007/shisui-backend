4a062d1a50c0791ec41366c7e34e2bac1b04e478
Sandeep Kumar <satyasandeep786@gmail.com>
2023-10-21 08:44:19 +0530

    feat: Edit telegram msg ?nftType

--- src/controllers/firebase.ts
+++ src/controllers/firebase.ts
@@ -2,92 +2,100 @@ const admin = require("firebase-admin");
 import { serviceCreds } from "../config/serviceAccount";
 
 admin.initializeApp({
-    credential: admin.credential.cert(serviceCreds),
-    databaseURL: "https://fir-project-1-58a04.firebaseio.com"
+  credential: admin.credential.cert(serviceCreds),
+  databaseURL: "https://fir-project-1-58a04.firebaseio.com",
 });
 
 let db = admin.firestore();
 
 export const addMessageReaction = async (item: any) => {
-    const messageReactionsCollection = await db.collection('messageReactions')
-    await messageReactionsCollection.add(item);
-}
+  const messageReactionsCollection = await db.collection("messageReactions");
+  await messageReactionsCollection.add(item);
+};
 
 export const editMessageReaction = async (item: any, messageId: string) => {
-    const messageReactionsCollection = await db.collection('messageReactions');
-    console.log(item, "Edit message");
+  const messageReactionsCollection = await db.collection("messageReactions");
+  console.log(item, "Edit message");
 
-    // Query for the document based on 'messageId'.
-    const querySnapshot = await messageReactionsCollection.where('messageId', '==', messageId).get();
+  // Query for the document based on 'messageId'.
+  const querySnapshot = await messageReactionsCollection
+    .where("messageId", "==", messageId)
+    .get();
 
-    // Check if the document exists.
-    if (!querySnapshot.empty) {
-        // Get the first document from the query (assuming 'messageId' is unique).
-        const docRef = querySnapshot.docs[0].ref;
+  // Check if the document exists.
+  if (!querySnapshot.empty) {
+    // Get the first document from the query (assuming 'messageId' is unique).
+    const docRef = querySnapshot.docs[0].ref;
 
-        // Update the document with the new data.
-        await docRef.update(item);
+    // Update the document with the new data.
+    await docRef.update(item);
 
-        console.log("Document successfully updated!");
-    } else {
-        console.log("No matching document found for the provided 'messageId'.");
-    }
-}
+    console.log("Document successfully updated!");
+  } else {
+    console.log("No matching document found for the provided 'messageId'.");
+  }
+};
 
 export const getMessageReaction = async (messageId: string) => {
-    const messageReactionsCollection = await db.collection('messageReactions');
-    const querySnapshot = await messageReactionsCollection.where('messageId', '==', messageId).get();
+  const messageReactionsCollection = await db.collection("messageReactions");
+  const querySnapshot = await messageReactionsCollection
+    .where("messageId", "==", messageId)
+    .get();
 
-    // Initialize an empty array to store the documents.
-    const docs: any = [];
+  // Initialize an empty array to store the documents.
+  const docs: any = [];
 
-    // Iterate through the query snapshot and add the data of each document to the 'docs' array.
-    querySnapshot.forEach((doc: any) => {
-        docs.push(doc.data()); // Add the data of the document.
-    });
+  // Iterate through the query snapshot and add the data of each document to the 'docs' array.
+  querySnapshot.forEach((doc: any) => {
+    docs.push(doc.data()); // Add the data of the document.
+  });
 
-    console.log(docs, "docs");
-    return docs[0]; // Return the array of documents.
-}
+  console.log(docs, "docs");
+  return docs[0]; // Return the array of documents.
+};
 
 export const getMessageReactionsById = async (userTelegramId: string) => {
-    const messageReactionsCollection = await db.collection('messageReactions');
-    const querySnapshot = await messageReactionsCollection.where('userTelegramId', '==', userTelegramId).get();
+  const messageReactionsCollection = await db.collection("messageReactions");
+  const querySnapshot = await messageReactionsCollection
+    .where("userTelegramId", "==", userTelegramId)
+    .get();
 
-    // Initialize an empty array to store the documents.
-    const docs: any = [];
+  // Initialize an empty array to store the documents.
+  const docs: any = [];
 
-    // Iterate through the query snapshot and add the data of each document to the 'docs' array.
-    querySnapshot.forEach((doc: any) => {
-        docs.push(doc.data()); // Add the data of the document.
-    });
+  // Iterate through the query snapshot and add the data of each document to the 'docs' array.
+  querySnapshot.forEach((doc: any) => {
+    docs.push(doc.data()); // Add the data of the document.
+  });
 
-    console.log(docs, "docs");
-    return docs; // Return the array of documents.
-}
+  console.log(docs, "docs");
+  return docs; // Return the array of documents.
+};
 
 export const addUserOtp = async (item: any) => {
-    const user = await db.collection('users')
-    await user.add(item);
-}
+  const user = await db.collection("users");
+  await user.add(item);
+};
 
 export const getUserOtpById = async (userTelegramId: string) => {
-    const user = await db.collection('users');
-    const querySnapshot = await user.where('userTelegramId', '==', userTelegramId).get();
+  const user = await db.collection("users");
+  const querySnapshot = await user
+    .where("userTelegramId", "==", userTelegramId)
+    .get();
 
-    // Initialize an empty array to store the documents.
-    const docs: any = [];
+  // Initialize an empty array to store the documents.
+  const docs: any = [];
 
-    // Iterate through the query snapshot and add the data of each document to the 'docs' array.
-    querySnapshot.forEach((doc: any) => {
-        docs.push(doc.data()); // Add the data of the document.
-    });
+  // Iterate through the query snapshot and add the data of each document to the 'docs' array.
+  querySnapshot.forEach((doc: any) => {
+    docs.push(doc.data()); // Add the data of the document.
+  });
 
-    console.log(docs, "docs");
-    return docs; // Return the array of documents.
-}
+  console.log(docs, "docs");
+  return docs; // Return the array of documents.
+};
 
 export const addNftReward = async (item: any) => {
-    const nftRewardsCollection = await db.collection('nftRewards')
-    await nftRewardsCollection.add(item);
-}
+  const nftRewardsCollection = await db.collection("nftRewards");
+  await nftRewardsCollection.add(item);
+};
--- src/utils/telegramBot.ts
+++ src/utils/telegramBot.ts
@@ -1,147 +1,154 @@
-import { addNftReward, editMessageReaction } from '../controllers/firebase';
-import { getMessageReaction } from '../controllers/firebase';
-import { addMessageReaction } from '../controllers/firebase';
+import { addNftReward, editMessageReaction } from "../controllers/firebase";
+import { getMessageReaction } from "../controllers/firebase";
+import { addMessageReaction } from "../controllers/firebase";
 
 interface MessageReaction {
-    dislikes: number;
-    likes: number;
-    messageId: string;
-    userFullName: string;
-    userId: string;
-    userTelegramId: string;
+  dislikes: number;
+  likes: number;
+  messageId: string;
+  userFullName: string;
+  userId: string;
+  userTelegramId: string;
 }
 
 export function setupBot(bot: any) {
-    console.log("Telegram Bot is started and running");
+  console.log("Telegram Bot is started and running");
 
-    bot.on('message', async (msg: any) => {
-        console.log(msg, "msg");
+  bot.on("message", async (msg: any) => {
+    console.log(msg, "msg");
 
-        const chatId = msg.chat.id;
-        const messageId = msg.message_id;
-        const userFullName = msg.from.first_name;
-        const userId = msg.from.id;
-        const userTelegramId = msg.from.username;
+    const chatId = msg.chat.id;
+    const messageId = msg.message_id;
+    const userFullName = msg.from.first_name;
+    const userId = msg.from.id;
+    const userTelegramId = msg.from.username;
 
-        // Message text
-        const messageText = 'How do you feel about this message?';
+    // Message text
+    const messageText = "How do you feel about this message?";
 
-        // Inline keyboard with reactions
-        const keyboard = {
-            inline_keyboard: [
-                [
-                    { text: '👍 Like', callback_data: 'like' },
-                    { text: '👎 Dislike', callback_data: 'dislike' }
-                ]
-            ]
-        };
+    // Inline keyboard with reactions
+    const keyboard = {
+      inline_keyboard: [
+        [
+          { text: "👍 Like", callback_data: "like" },
+          { text: "👎 Dislike", callback_data: "dislike" },
+        ],
+      ],
+    };
 
-        // Send message with inline keyboard
-        bot.sendMessage(chatId, messageText, {
-            reply_markup: JSON.stringify(keyboard),
-            reply_to_message_id: messageId
-        });
-
-        // Add new message reaction object to Firebase
-        const reactionItem: MessageReaction = {
-            dislikes: 0,
-            likes: 0,
-            messageId: messageId.toString(),
-            userFullName,
-            userId: userId.toString(),
-            userTelegramId
-        };
-
-        try {
-            console.log(reactionItem, "reactionItem");
-            await addMessageReaction(reactionItem);
-        } catch (error) {
-            console.error('Error adding message reaction to Firebase:', error);
-        }
-
+    // Send message with inline keyboard
+    bot.sendMessage(chatId, messageText, {
+      reply_markup: JSON.stringify(keyboard),
+      reply_to_message_id: messageId,
     });
 
-    // Handle callback queries
-    bot.on('callback_query', async (query: any) => {
-        const chatId = query.message.chat.id;
-        const messageId = query.message.reply_to_message.message_id;
-        const messageIdTwo = query.message.message_id;
-        const reaction = query.data;
+    // Add new message reaction object to Firebase
+    const reactionItem: MessageReaction = {
+      dislikes: 0,
+      likes: 0,
+      messageId: messageId.toString(),
+      userFullName,
+      userId: userId.toString(),
+      userTelegramId,
+    };
 
-        // Find the message reaction object in Firebase based on messageId
-        const messageReaction = await getMessageReaction(messageId.toString());
+    try {
+      console.log(reactionItem, "reactionItem");
+      await addMessageReaction(reactionItem);
+    } catch (error) {
+      console.error("Error adding message reaction to Firebase:", error);
+    }
+  });
 
-        if (messageReaction) {
-            // Handle different reactions
-            if (reaction === 'like') {
-                messageReaction.likes++;
-                // Check if the number of likes is 10 or more
-                const userChatId = messageReaction.userId;
-                const userTelegramId = messageReaction.userTelegramId;
+  // Handle callback queries
+  bot.on("callback_query", async (query: any) => {
+    const chatId = query.message.chat.id;
+    const messageId = query.message.reply_to_message.message_id;
+    const messageIdTwo = query.message.message_id;
+    const reaction = query.data;
 
-                if (messageReaction.likes === 5) {
-                    await addNftReward({
-                        star: 1,
-                        userChatId,
-                        userTelegramId,
-                        isClaimed: false
-                    })
-                    // Send a message to the user who sent the original message
-                    bot.sendMessage(userChatId, `🎉 Congratulations! Your message received 5 likes! 🌟 You've been rewarded with a 1-star NFT! Claim your NFT now at http://localhost:4000/home. Hurry, it's waiting just for you! 🚀`);
-                }
-                else if (messageReaction.likes === 10) {
-                    await addNftReward({
-                        star: 3,
-                        userChatId,
-                        userTelegramId,
-                        isClaimed: false
-                    })
-                    // Send a message to the user who sent the original message
-                    bot.sendMessage(userChatId, `🎉 Congratulations! Your message received 10 likes! 🌟 You've been rewarded with a 3-star NFT! Claim your NFT now at http://localhost:4000/home. Hurry, it's waiting just for you! 🚀`);
-                }
-                else if (messageReaction.likes === 20) {
-                    await addNftReward({
-                        star: 5,
-                        userChatId,
-                        userTelegramId,
-                        isClaimed: false
-                    })
-                    // Send a message to the user who sent the original message
-                    bot.sendMessage(userChatId, `🎉 Congratulations! Your message received 20 likes! 🌟 You've been rewarded with a 5-star NFT! Claim your NFT now at http://localhost:4000/home. Hurry, it's waiting just for you! 🚀`);
-                }
-            } else if (reaction === 'dislike') {
-                messageReaction.dislikes++;
-            }
+    // Find the message reaction object in Firebase based on messageId
+    const messageReaction = await getMessageReaction(messageId.toString());
 
-            // Update the inline keyboard with the new like and dislike count
-            const messageText = `How do you feel about this message?`;
-            const keyboard = {
-                inline_keyboard: [
-                    [
-                        { text: `👍 Like ${messageReaction.likes}`, callback_data: 'like' },
-                        { text: `👎 Dislike ${messageReaction.dislikes}`, callback_data: 'dislike' }
-                    ]
-                ]
-            };
+    if (messageReaction) {
+      // Handle different reactions
+      if (reaction === "like") {
+        messageReaction.likes++;
+        // Check if the number of likes is 10 or more
+        const userChatId = messageReaction.userId;
+        const userTelegramId = messageReaction.userTelegramId;
 
-            // Edit the original message to update the inline keyboard
-            bot.editMessageText(messageText, {
-                chat_id: chatId,
-                message_id: messageIdTwo,
-                reply_markup: JSON.stringify(keyboard)
-            });
-
-            // Answer the callback query to remove the "loading" status
-            bot.answerCallbackQuery(query.id);
-
-            // Update message reaction object in Firebase
-            try {
-                await editMessageReaction(messageReaction, messageId.toString());
-            } catch (error) {
-                console.error('Error updating message reaction in Firebase:', error);
-            }
+        if (messageReaction.likes === 5) {
+          await addNftReward({
+            star: 1,
+            userChatId,
+            userTelegramId,
+            isClaimed: false,
+          });
+          // Send a message to the user who sent the original message
+          bot.sendMessage(
+            userChatId,
+            `🎉 Congratulations! Your message received 5 likes! 🌟 You've been rewarded with a 1-star NFT! Claim your NFT now at http://localhost:4000/home?nftType=1. Hurry, it's waiting just for you! 🚀`
+          );
+        } else if (messageReaction.likes === 10) {
+          await addNftReward({
+            star: 3,
+            userChatId,
+            userTelegramId,
+            isClaimed: false,
+          });
+          // Send a message to the user who sent the original message
+          bot.sendMessage(
+            userChatId,
+            `🎉 Congratulations! Your message received 10 likes! 🌟 You've been rewarded with a 3-star NFT! Claim your NFT now at http://localhost:4000/home?nftType=3. Hurry, it's waiting just for you! 🚀`
+          );
+        } else if (messageReaction.likes === 20) {
+          await addNftReward({
+            star: 5,
+            userChatId,
+            userTelegramId,
+            isClaimed: false,
+          });
+          // Send a message to the user who sent the original message
+          bot.sendMessage(
+            userChatId,
+            `🎉 Congratulations! Your message received 20 likes! 🌟 You've been rewarded with a 5-star NFT! Claim your NFT now at http://localhost:4000/home?nftType=5. Hurry, it's waiting just for you! 🚀`
+          );
         }
-    });
+      } else if (reaction === "dislike") {
+        messageReaction.dislikes++;
+      }
 
+      // Update the inline keyboard with the new like and dislike count
+      const messageText = `How do you feel about this message?`;
+      const keyboard = {
+        inline_keyboard: [
+          [
+            { text: `👍 Like ${messageReaction.likes}`, callback_data: "like" },
+            {
+              text: `👎 Dislike ${messageReaction.dislikes}`,
+              callback_data: "dislike",
+            },
+          ],
+        ],
+      };
 
+      // Edit the original message to update the inline keyboard
+      bot.editMessageText(messageText, {
+        chat_id: chatId,
+        message_id: messageIdTwo,
+        reply_markup: JSON.stringify(keyboard),
+      });
+
+      // Answer the callback query to remove the "loading" status
+      bot.answerCallbackQuery(query.id);
+
+      // Update message reaction object in Firebase
+      try {
+        await editMessageReaction(messageReaction, messageId.toString());
+      } catch (error) {
+        console.error("Error updating message reaction in Firebase:", error);
+      }
+    }
+  });
 }
